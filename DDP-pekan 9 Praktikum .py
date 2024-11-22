c6832118e447541913b8028e12e940f641d5f5cb
Maulidya Syifa Aszahra <maulidyasyifaazzahra@gmail.com>
2024-11-22 10:11:58 +0700

    Praktikum 9

---
+++ DDP-pekan 9 Praktikum .py
@@ -0,0 +1,41 @@
+print()
+print('## Nomor 1 ##')
+
+def celcius_ke_fahrenheit(celcius):
+    konversi=(celcius*9/5)+32
+    return konversi
+
+print(celcius_ke_fahrenheit(0))
+print(celcius_ke_fahrenheit(100))
+
+print()
+print('## Nomor 2 ##')
+
+def ganjil_genap(bilangan):
+    penentu=bilangan % 2 == 0
+    return penentu
+
+print(ganjil_genap(4)) #true
+print(ganjil_genap(7)) #false
+
+print()
+print('## Nomor 3 ##')
+
+def nilai(keterangan):
+    if keterangan >= 80:
+        print("Lulus")
+    elif keterangan <= 60:
+        print("Gagal")
+    else:
+        print("tidak valid")
+    
+nilai(80)
+nilai(60)
+
+print()
+print('## Nomer 4 ##')
+
+def bilangan_ganjil(ganjil):
+    return[i for i in range(1,ganjil,2)]
+
+print(bilangan_ganjil(20))
